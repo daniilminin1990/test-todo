47a2d80910cb6106fbd9d050287577b29d007029
Daniil <daniilminin1990@yandex.ru>
2024-01-28 23:05:23 +0600

    todol 3 lesson: removeTask (сделан заранее еще раньше), noTasks => 'Nothing to show',

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -34,26 +34,28 @@ export const Todolist = (props: TodolistProps) => {
       </h3>
       <input value={newTaskTitle} onChange={onNewTitleChangeHandler} onKeyDown={onEnterAddTask} />
       <button onClick={onClickAddTaskHandler}>+</button>
-      <ul>
-        {
-          props.tasks.map(t => {
-            const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-              let checked = e.currentTarget.checked
-              props.changeTaskStatus(t.id, checked)
-            }
-            const onClickHandler = () => {
-              props.removeTask(t.id)
-            }
-            return (
-              <li key={t.id}>
-                <input type="checkbox" checked={t.isDone} onChange={onChangeHandler} />
-                <span>{t.taskTitle}</span>
-                <button onClick={onClickHandler}>x</button>
-              </li>
-            )
-          })
-        }
-      </ul>
+      {props.tasks.length === 0
+        ? <p>Nothing to show</p>
+        : <ul>
+          {
+            props.tasks.map(t => {
+              const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+                let checked = e.currentTarget.checked
+                props.changeTaskStatus(t.id, checked)
+              }
+              const onClickHandler = () => {
+                props.removeTask(t.id)
+              }
+              return (
+                <li key={t.id}>
+                  <input type="checkbox" checked={t.isDone} onChange={onChangeHandler} />
+                  <span>{t.taskTitle}</span>
+                  <button onClick={onClickHandler}>x</button>
+                </li>
+              )
+            })
+          }
+        </ul>}
       <button onClick={() => props.changeTasksFilter('all')}>All</button>
       <button onClick={() => props.changeTasksFilter('active')}>Active</button>
       <button onClick={() => props.changeTasksFilter('completed')}>Completed</button>
