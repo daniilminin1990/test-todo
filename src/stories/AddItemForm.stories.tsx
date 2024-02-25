2e85ef7578c7ff79cf29b20c7c1b5e9139755d4f
Daniil <daniilminin1990@yandex.ru>
2024-02-25 21:24:01 +0600

    НачÐСтартовая точка 4 спринтаÑ

--- src/stories/AddItemForm.stories.tsx
+++ src/stories/AddItemForm.stories.tsx
@@ -75,6 +75,7 @@ export const AddItemFormError = (props: AddItemFormProps) => {
     </div>
   );
 };
+//! Тут начался 4 спринт
 
 // // Вид с ошибкой № 2
 // const AddItemFormError1 = (props: AddItemFormProps) => {
