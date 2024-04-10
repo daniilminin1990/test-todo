d520738a0ce1494ab8ee51895ee89f7f3d9ea7de
Daniil <daniilminin1990@yandex.ru>
2024-04-11 03:39:14 +0500

    refactor removed title node in ThemeToggler

--- src/components/ThemeTogglerSlider.tsx
+++ src/components/ThemeTogglerSlider.tsx
@@ -22,12 +22,8 @@ export const ThemeTogglerSlider = () => {
     //     p: 3,
     //   }}
     // >
-    <>
-      {theme.palette.mode} mode
       <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
         {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
       </IconButton>
-    </>
-    // </Box>
   );
 }
