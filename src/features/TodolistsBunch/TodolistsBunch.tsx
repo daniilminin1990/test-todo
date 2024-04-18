4200f68e2efb270722db7c6f092d8583e6285dab
Daniil <daniilminin1990@yandex.ru>
2024-04-18 22:38:33 +0500

    add pagination bar PROBLEM with makeStyles

--- src/features/TodolistsBunch/TodolistsBunch.tsx
+++ src/features/TodolistsBunch/TodolistsBunch.tsx
@@ -4,13 +4,18 @@ import {
   FilterValuesType,
   todolistsSelectors,
 } from "../../redux/todolistsSlice";
-import { Grid } from "@mui/material";
+import { ClassNameMap, Grid, Pagination, Paper } from "@mui/material";
 import { Todolist } from "./Todolist/Todolist";
 import { Navigate, useSearchParams } from "react-router-dom";
 import { appSelectors } from "../../redux/appSlice";
 import { loginSelectors } from "../../redux/loginSlice";
 import { AddItemForm } from "../../common/components";
 import { useActions } from "../../common/hooks/useActions";
+import Stack from "@mui/material/Stack";
+import { useTheme } from "@mui/material/styles";
+import { grey, yellow } from "@mui/material/colors";
+import { CSSProperties } from "@mui/material/styles/createMixins";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 
 type TodolistsBunchProps = {};
 export const TodolistsBunch: React.FC<TodolistsBunchProps> = () => {
@@ -25,6 +30,24 @@ export const TodolistsBunch: React.FC<TodolistsBunchProps> = () => {
   } = useActions();
 
   const [searchParams, setSearchParams] = useSearchParams();
+  const theme: any = useTheme();
+
+  const useStyles1: any = makeStyles(() =>
+    createStyles({
+      ul: {
+        "& .MuiPaginationItem-root": {
+          backgroundColor: "#ffffff",
+          color: "#171717",
+
+          "&.Mui-selected": {
+            backgroundColor: "#6c00ea",
+          },
+        } as CSSProperties,
+      },
+    })
+  );
+
+  const classes1: ClassNameMap<string> = useStyles1();
 
   const todolists = useAppSelector((state) =>
     todolistsSelectors.todolists(state)
@@ -92,6 +115,17 @@ export const TodolistsBunch: React.FC<TodolistsBunchProps> = () => {
 
   return (
     <>
+      <Stack spacing={2} direction={"column"}>
+        {theme.palette.mode === "dark" ? (
+          <div className={classes1.ul}>
+            <Pagination count={10} shape="rounded" variant="outlined" />
+          </div>
+        ) : (
+          <div className={classes1.ul}>
+            <Pagination count={10} shape="rounded" variant="outlined" />
+          </div>
+        )}
+      </Stack>
       <Grid container style={{ padding: "20px" }}>
         <AddItemForm callback={addTodo} />
       </Grid>
